d159007570c18fa5247460c48fb799cf49006af8
codewizard-2004 <amalv2004@gmail.com>
2025-12-18 20:03:24 +0530

    updated backend url

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const API_URL = "http://127.0.0.1:8000";
+export const API_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://127.0.0.1:8000";
 
 export const MODELS = [
     { id: "resnet18", name: "ResNet 18" },
